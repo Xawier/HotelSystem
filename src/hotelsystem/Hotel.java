773654f1e7bac520e1a92d3b28fdcf4c650797d8
Mateusz Haber <mateusz.haber@uj.edu.pl>
2014-10-20 13:24:07 +0200

    Hotel classes

---
+++ src/hotelsystem/Hotel.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotelsystem;
+
+import java.util.Calendar;
+import java.util.List;
+
+/**
+ *
+ * @author Mateusz
+ */
+public class Hotel {
+    void add(Room room){
+
+    }
+    Room room(String name){
+        return null;
+    }
+    
+     List<QueryResult> findFreeRooms(Calendar start, Calendar end, int n_persons){
+       return null;  
+     }
+}
